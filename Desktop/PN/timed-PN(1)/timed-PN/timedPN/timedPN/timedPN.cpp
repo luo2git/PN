265dc72b0b5e74c4278c9c95e658b7a4fbd11684
starfen <nieweiyu@163.com>
2019-10-29 16:22:09 +0800

    PY tree

--- Desktop/PN/timed-PN(1)/timed-PN/timedPN/timedPN/timedPN.cpp
+++ Desktop/PN/timed-PN(1)/timed-PN/timedPN/timedPN/timedPN.cpp
@@ -26,13 +26,13 @@ int main()
 {
 	InitSMatrix(&C_pre);   
 	printf("创建前置关联矩阵C_pre: ");
-	CreateSMatrix(&C_pre, "C:\\Users\\A237\\Desktop\\新建文件夹 - 副本\\timed-PN(1)\\timed-PN\\C_pref.txt");
+	CreateSMatrix(&C_pre, "C:\\Users\\A237\\Desktop\\PN\\timed-PN(1)\\timed-PN\\C_pref.txt");
 	OutputSMatrix(C_pre);  
 	printf("\n");
 	
 	InitSMatrix(&C_post);
 	printf("创建后置关联矩阵C_post: ");
-	CreateSMatrix(&C_post, "C:\\Users\\A237\\Desktop\\新建文件夹 - 副本\\timed-PN(1)\\timed-PN\\C_postf.txt");
+	CreateSMatrix(&C_post, "C:\\Users\\A237\\Desktop\\PN\\timed-PN(1)\\timed-PN\\C_postf.txt");
 	OutputSMatrix(C_post);   
 	printf("\n");
 
